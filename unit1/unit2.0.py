0a9bbfa011ca1f1154d8093f921966b391107a87
hoyul1845 <hoyul1845@naver.com>
2022-10-16 13:53:07 +0900

    unit 2.0파이썬 설치하기

---
+++ unit1/unit2.0.py
@@ -0,0 +1,39 @@
+# 파이썬 설치하기
+ # 실습하려면 파이썬을 설치
+ # 파이썬은 Windows, 리눅스, macOS에서 설치
+# Windows에 파이썬 설치하기
+ # 웹 브라우저를 실행하고 다음 주소로 이동 파이썬 공식 사이트http://www.python.org/downloads/
+ # 웹사이트가 표시되면 Download Python 3.6.0 버튼을 클릭
+ # 여기서 Add Python 3.6 to PATH는 명령 프롬프트 어디서나 파이썬을 실행
+ # 사용자 계정 컨트롤 창이 표시되면 예를 클릭
+ # 파이썬 설치
+ # 파이썬 설치가 끝
+ # Disable path length limit를 클릭
+ # Windows는 파일 이름과 경로의 길이가 260자로 제한
+ # 260자를 넘더라도 문제가 발생하지 않도록 길이 제한을 해제
+ # 사용자 계정 컨트롤 창이 표시되면 예를 클릭
+# IDLE 실행
+ # 파이썬이 정상적으로 설치되었는 지 확인하기 위해 IDLE을 실행
+ # Windows 시작 버튼을 클릭한 뒤 Python 3.6 > IDLE (Python 3.6 32-bit)을 실행
+ # IDLE은 통합 개발 학습 환경(Integrated Development and Learning Environment)의 약어
+ # IDLE이 실행 >>> 뒤에 커서가 깜빡이면 파이썬이 정상적으로 설치
+# 프로젝트 폴더 생성
+ # 파이썬 소스 파일(스크립트 파일)을 저장할 폴더 C:\project를 생성
+ # 탐색기를 실행하고 로컬 디스크 (C:)로 이동
+ # 마우스 오른쪽 버튼을 누르면 팝업 메뉴가 나오는데, 여기서 새로 만들기(W) > 폴더(F)를 클릭한 뒤 폴더 이름에 project를 입력하여 폴더를 생성
+# 리눅스에 파이썬 설치하기
+ # 우분투 리눅스 최신 버전은 파이썬 3가 미리 설치
+ # 파이썬을 실행하려면 다음과 같이 python3를 입력
+ # CentOS는 파이썬 3가 아닌 2.7이 설치되어 있음
+ # 파이썬 3를 설치하려면 다음과 같이 명령을 입력
+ # 리눅스 배포판의 패키지 대신 파이썬 소스 코드를 직접 컴파일하여 설치할 수도 있음
+ # 먼저 C 컴파일러와 빌드 도구를 설치
+ # 파이썬 소스 코드를 받아서 컴파일 한 뒤 설치
+ # 리눅스에서 IDLE을 사용하려면 TkInter를 설치
+# macOS에 파이썬 설치하기
+ # macOS는 다음 주소에서 Max OS X 64-bit/32-bit installer를 받아서 설치
+  # Python Releases for Mac OS X
+  # https://www.python.org/downloads/mac-osx/
+# 참고 
+ # 파이썬은 IDLE 이외에도 각자 취향에 맞는 코드 에디터(텍스트 편집기)를 사용하여 프로그래밍할 수 있음
+ # Visual Studio Code: 마이크로소프트에서 제공하는 오픈 소스 코드 에디터
